55e3418d35c63204693946cb5cb0d28a30e7a758
brushieF <bruziej@gmail.com>
2019-01-14 23:51:46 +0100

    Init

---
+++ WebApplication4/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+
+using System.Web.Http.Cors;
+
+namespace WebApplication4
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Konfiguracja i usługi składnika Web API
+            // Skonfiguruj składnik Web API, aby korzystał tylko z uwierzytelniania za pomocą tokenów bearer.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            //EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "GET,POST");
+
+            //config.EnableCors(cors);
+            // Trasy składnika Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
